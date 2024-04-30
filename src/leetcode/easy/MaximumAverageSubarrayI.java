d29d78992f0d8e042ec7764467075b4bdbf6dc56
Luka Signe--Morice <luka@signemorice.com>
2024-04-30 22:31:26 +0200

    Max avg subarray

---
+++ src/leetcode/easy/MaximumAverageSubarrayI.java
@@ -0,0 +1,19 @@
+package leetcode.easy;
+
+public class MaximumAverageSubarrayI {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+
+        for(int i = 0; i<k; i++){
+            sum += nums[i];
+        }
+
+        double max = sum;
+        for(int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i-k];
+            if(sum > max) max = sum;
+        }
+
+        return max / k;
+    }
+}
